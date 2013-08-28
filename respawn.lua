6aed9344dad04aad87eeb41e61eb0c7f9d587818
vice655 <zeusmurphy@hotmail.com>
2013-08-28 19:50:54 +0100

    changed a few shit made it more "Code Efficient"

--- respawn.lua
+++ respawn.lua
@@ -1,6 +1,6 @@
 -- The following code was created for TTT, using it in any other gamemode may have undesired consequences, although unlikely.
 -- Version 0.93
-local CATEGORY_NAME = "TTT Commands" -- What you are playing
+local CATEGORY_NAME = "TTT Debugging" -- What you are playing
 
 function ulx.spawn( calling_ply, target_plys )
 	local affected_plys = {}
@@ -13,7 +13,7 @@ function ulx.spawn( calling_ply, target_plys )
 		elseif v:IsFrozen() then
 			ULib.tsayError( calling_ply, v:Nick() .. " is apparantly frozen?", true )
 		elseif v:Alive() then
-			ULib.tsayError( calling_ply, v:Nick() .. " is alive? Do you think i'm an idiot -- how can I respawn him?!", true ) -- I wonder if you could use csay? :o
+			ULib.tsayError( calling_ply, v:Nick() .. " is alive? Do you think i'm an idiot -- how can I respawn him?! Even the Law cant respawn him.", true ) -- I wonder if you could use csay? :o
 		else
 			v:SetRole(ROLE_INNOCENT) -- They will be innocent
 			v:Spawn()
@@ -23,7 +23,7 @@ function ulx.spawn( calling_ply, target_plys )
 
 	ulx.fancyLogAdmin( calling_ply, "#A spawned the player -  #T", affected_plys )
 end
-local law = ulx.command( CATEGORY_NAME, "ulx spawn", ulx.spawn, "!spawn" )
+local law = ulx.command( CATEGORY_NAME, "ulx spawn", ulx.spawn, "!spawn" ) -- the command name
 law:addParam{ type=ULib.cmds.PlayersArg }
 law:defaultAccess( ULib.ACCESS_SUPERADMIN )
 law:help( "Respawns <user> and makes them innocent" )
@@ -39,27 +39,27 @@ function ulx.cc_traitor( ply, targs )
  local silent = ulx.command( CATEGORY_NAME, "ulx forcetraitor", ulx.cc_traitor, "!forcetraitor", true )
  silent:addParam{type=ULib.cmds.PlayersArg, hint = "<name(s)>"}
  silent:defaultAccess( ULib.ACCESS_SUPERADMIN )
- silent:help( "Turns desired user into a traitor" )
+ silent:help( "Turns desired user into a traitor" ) -- ulx help hint
 
 function ulx.cc_detective( ply, targs )
 
  	for _, v in ipairs( targs ) do
 		 v:SetRole(ROLE_DETECTIVE)
-		 v:Give("weapon_ttt_wtester")
+		 v:Give("weapon_ttt_wtester") -- give DNA scanner
 		 v:SetDefaultCredits()
  	end
  	SendFullStateUpdate()
  end
  local silent = ulx.command( CATEGORY_NAME, "ulx forcedetective", ulx.cc_detective, "!forcedetective", true )
  silent:addParam{type=ULib.cmds.PlayersArg, hint = "<name(s)>"}
- silent:defaultAccess( ULib.ACCESS_SUPERADMIN )
+ silent:defaultAccess( ULib.ACCESS_SUPERADMIN ) -- Access only available to SA+
  silent:help( "Turns desired user into a detective." )
 
 
 function ulx.cc_innocent( ply, targs )
 
  	for _, v in ipairs( targs ) do
-		 v:SetRole(ROLE_INNOCENT)
+		 v:SetRole(ROLE_INNOCENT) -- Forces innocent status
  	end
  	SendFullStateUpdate()
  end
